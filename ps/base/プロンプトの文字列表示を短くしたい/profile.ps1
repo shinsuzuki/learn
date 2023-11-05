8444eb92507865a99b58e94a70ce8a177fad39e8
shin.suzuki <shin.suzuki.dev@gmail.com>
2023-11-05 12:00:24 +0900

    プロンプト短縮

--- ps/base/プロンプトの文字列表示を短くしたい/profile.ps1
+++ ps/base/プロンプトの文字列表示を短くしたい/profile.ps1
@@ -1,6 +1,13 @@
 #
 # 保存先 → C:\Documents and Settings\＜ユーザー名＞\My Documents\WindowsPowerShell\pfofile.ps1
 #
-function prompt() {
-    "[" + (Split-Path (Get-Location) -Leaf) + "] > "
+
+# プロンプト短縮1
+# function prompt() {
+#     "[" + (Split-Path (Get-Location) -Leaf) + "] > "
+# }
+
+# プロンプト短縮2
+function prompt
+    {"PS " + (get-location).drive.name+":\...\"+ $( ( get-item $pwd ).Name ) +">"
 }
