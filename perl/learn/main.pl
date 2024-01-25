959f62e852ab36beb932d1113b5f6a6b097dfee5
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-01-26 00:25:40 +0900

    関数の書き方

--- perl/learn/main.pl
+++ perl/learn/main.pl
@@ -1,1 +1,19 @@
-print("test")
+print("test\n");
+
+
+# 関数定義
+sub add {
+    my($x, $y) = @_;
+    return $x + $y;
+}
+
+print add(1,2)."\n";
+print add(1,2)."\n";
+
+
+my $text = "abc123";
+if ($text =~ /^a/) {
+    print("match")
+}
+
+
