bcc01b657dee1f82b9b425e42a6906a77e21d5d9
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-01-30 00:42:42 +0900

    pythonでの正規表現。

---
+++ python/features/regex/main.py
@@ -0,0 +1,33 @@
+﻿import re
+
+
+def main():
+    # 正規表現の書き方
+    targetAlp = "abcde"
+    targetNum = "12345"
+
+    print("====> re")
+    if re.match(r"^[a-z]", targetAlp):
+        print("alphabet ok.")
+    if re.match(r"^[a-z]", targetNum):
+        print("alphabet ng")
+
+    print("====> re.compile")
+    regex = re.compile(r"^[a-z]")
+    if regex.match(targetAlp):
+        print("alphabet ok")
+
+    if regex.match(targetNum):
+        print("alphabet ng")
+
+
+if __name__ == "__main__":
+    main()
+
+
+# > python main.py
+#
+# ====> re
+# alphabet ok.
+# ====> re.compile
+# alphabet ok
