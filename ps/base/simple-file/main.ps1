c3285c351070c2c7e85770e7ccf5fd5b17c3d3e1
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-01-30 12:10:14 +0900

    powershell ファイルシンプル操作

---
+++ ps/base/simple-file/main.ps1
@@ -0,0 +1,8 @@
+﻿$outlist = @()
+
+Get-Content -path "./sample_utf8.txt" -Encoding UTF8 |
+    ForEach-Object {
+        $outlist += $_
+    }
+
+Set-Content -path "./sample_output_utf8.txt" -Value $outlist -Encoding UTF8
