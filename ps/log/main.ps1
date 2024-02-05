6574a07749fbd9ee6369de938fabf6dec583fc00
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-02-05 18:42:17 +0900

    ログ解析

---
+++ ps/log/main.ps1
@@ -0,0 +1,17 @@
+﻿#
+$list = @()
+Import-Csv .\log.csv -Encoding UTF8 | ForEach-Object {
+    if ($_.contact_type.Trim() -eq "phone" -and $_.urgency.Trim() -eq "1 High") {
+        $list += $_
+    }
+}
+
+$list = $list | Sort-Object { $_.priority }
+$outputList = @()
+foreach ($item in $list) {
+    $line = $item.sys_created_on + ", [" + $item.priority + "], " + $item.category + ", " + $item.state
+    Write-Host $line
+    $outputList += $line
+}
+
+Set-Content -Path result.txt -Encoding UTF8 -Value $outputList
