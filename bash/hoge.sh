893c9d2d5970540042ce15fb5e51f29dd5a5063f
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-02-18 18:50:37 +0900

    formatter

--- bash/hoge.sh
+++ bash/hoge.sh
@@ -14,26 +14,24 @@ echo "script_args_count:$#" # 引数の数
 # echo "script_args_2:$2"     # 2つ目の引数
 # echo "script_args_all:$@"   # 全ての引数
 
-
 #____________________ 文字列
 echo "____文字列"
 name="john_abcdefg"
 echo $name
 
 #________ 文字列展開
-echo "xyz_${name}"      # xyz_john_abcdefg
+echo "xyz_${name}" # xyz_john_abcdefg
 
 #________ 部分文字列
-echo "部分文字列:${name:0:3}"        # joh
-echo "部分文字列:${name:4}"          # _abcdefg
+echo "部分文字列:${name:0:3}" # joh
+echo "部分文字列:${name:4}"   # _abcdefg
 
 #________ 長さ
-echo "length:${#name}"         # 12
+echo "length:${#name}" # 12
 
 #________ uppper,lower
-echo "upper:${name^^}"        # JOHN_ABCDEFG
-echo "lower:${name,,}"        # john_abcdefg
-
+echo "upper:${name^^}" # JOHN_ABCDEFG
+echo "lower:${name,,}" # john_abcdefg
 
 #____________________ デフォルト引数
 foo=""
@@ -44,11 +42,10 @@ echo "____コマンド実行"
 pwd
 echo "dir:$(pwd)"
 
-
 #____________________ 関数
 echo "____関数"
 #________ 文字列を返すイメージ
-function get_name () {
+function get_name() {
     #echo "arg_count:$#"    # 引数の数
     echo "$1 - $2"
 }
@@ -58,7 +55,7 @@ myname=$(get_name "john" "punch")
 echo "myname:$myname"
 
 #________ 数値を返す
-function get_value () {
+function get_value() {
     return 100
 }
 
@@ -66,22 +63,19 @@ function get_value () {
 get_value
 echo "ret:$?"
 
-
 #____________________ パス
 echo "____パス"
 path="/abc/def/ghi.cs"
-echo "${path##*/}"      # ファイル名
-echo "${path%/*}"       # DIR名
-
+echo "${path##*/}" # ファイル名
+echo "${path%/*}"  # DIR名
 
 #____________________ 色付け
 echo "____色付け"
 # https://linux.just4fun.biz/?%E9%80%86%E5%BC%95%E3%81%8D%E3%82%B7%E3%82%A7%E3%83%AB%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88/%E3%83%A1%E3%83%83%E3%82%BB%E3%83%BC%E3%82%B8%E3%81%AB%E8%89%B2%E3%82%92%E4%BB%98%E3%81%91%E3%82%8B%E3%83%BBtput
-tput setaf 1 && "hello world red"   # red
-tput setaf 2 && "hello world green"   # grenn
-tput setaf 3 && "hello world yellow"   # yellow
-tput setaf 7 && "hello world white"   # white
-
+tput setaf 1 && "hello world red"    # red
+tput setaf 2 && "hello world green"  # grenn
+tput setaf 3 && "hello world yellow" # yellow
+tput setaf 7 && "hello world white"  # white
 
 #____________________ 制御
 echo "____制御"
@@ -121,20 +115,21 @@ fi
 echo "__case"
 cv="v2"
 case "$cv" in
-    "v1") echo case_v1
+"v1")
+    echo case_v1
     ;;
-    "v2"|"v3") echo case v2 or v3
+"v2" | "v3")
+    echo case v2 or v3
     ;;
-    *) echo default
+*)
+    echo default
     ;;
 esac
 
-
 #____________________ 配列
 echo "____配列"
-fruits=('apple' 'banan' 'orange')   # 丸かっこで定義、
-echo "fruits-2:${fruits[2]}"    # orange、参照は波括弧
-
+fruits=('apple' 'banan' 'orange') # 丸かっこで定義、
+echo "fruits-2:${fruits[2]}"      # orange、参照は波括弧
 
 echo "__宣言"
 declare -a item_array=()
@@ -146,7 +141,6 @@ echo "item:${item_array[1]}"
 for item in "${item_array[@]}"; do
     echo "for-item:$item"
 done
-
 
 #____________________ 辞書
 echo "____辞書"
@@ -168,13 +162,11 @@ for value in "${dic[@]}"; do
     echo "value:$value"
 done
 
-
 #____________________ ファイル読込
 echo "____ファイル読込"
 while read -r line; do
     echo "$line"
-done < file.txt
-
+done <file.txt
 
 #____________________ ディレクトリ操作
 # echo "____ディレクトリ操作"
@@ -184,13 +176,11 @@ done < file.txt
 #ディレクトリを階層、中身が存在していても消す
 # rm -rf "a"
 
-
 #____________________ ユーザー入力の入力待ち
 # echo "____ユーザー入力の入力待ち"
 # echo -n "proceed? [y/n]:"
 # read -r ans
 # echo "$ans"
-
 
 #____________________ コマンドの結果(forは使い所による)
 echo "____コマンドの結果"
@@ -198,7 +188,6 @@ echo "____コマンドの結果"
 # for item in $(ls -1 ./*.txt); do
 #     echo "item:$item"
 # done;
-
 
 echo "__ファイル一覧"
 for file in *.txt; do
@@ -230,12 +219,11 @@ outpufile="output_psaux.txt"
 rm $outpufile
 for item in "${ps_array[@]}"; do
     echo "item:$item"
-    echo "$item" >> $outpufile
+    echo "$item" >>$outpufile
 done
 
 echo "__コマンドの出力結果を文字列で出力"
 echo "dir:$(pwd)"
-
 
 #____________________ 他コマンドとの連携
 echo "____他コマンドとの連携"
@@ -252,8 +240,6 @@ ps -aux | grep systemd
 echo "__sed(置換、削除、抽出など)"
 ps aux | sed s/systemd/SYSTEMD/g
 
-
-
 #____________________ 設定ファイルから読込み
 echo "____設定ファイルから読込み"
 # shellcheck disable=SC1091
@@ -261,8 +247,6 @@ source ./setting.config
 tput setaf 2 && echo "MY_VALUE1:$MY_VALUE1"
 tput setaf 2 && echo "MY_VALUE2:$MY_VALUE2"
 tput setaf 7
-
-
 
 echo ""
 echo "__end bash__"
