e9caec5149e78401a0f6b9905e012212788cdf1d
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-02-22 18:21:05 +0900

    encoding

--- ps/grep/main.ps1
+++ ps/grep/main.ps1
@@ -1,6 +1,6 @@
 ﻿Write-Host "____start"
 
-Select-String -Path .\sample.log -Pattern "^INC[0-9].+,2020-02-03"
+Select-String -Path .\sample.log -Pattern "^INC[0-9].+,2020-02-03" | Out-File -enco utf8 result.txt
 
 Write-Host "____end"
 
