4966322795a92d677f771b9b3361377680a689a5
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-07-24 19:51:26 +0900

    bcp

--- sqlserver/bcp/bcp-export.bat
+++ sqlserver/bcp/bcp-export.bat
@@ -5,6 +5,3 @@ rem [/T]:                    Windows認証
 rem [/c]:                    テキスト形式で出力
 rem [/t ","]                 区切り文字を指定
 bcp MySample.dbo.customer out output.csv /S localhost /T /c /t ","
-
-
-bcp "select * from MySample.dbo.customer where id between 2 and 4" queryout qoutput.csv /S localhost /T /c /t ","
