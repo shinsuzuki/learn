9bd0aa2ef797b6d72b9ea823187f36a8f3aa316b
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-07-24 18:35:35 +0900

    sqlcmd update

--- sqlserver/sqlcmd/test-sqlcmd.bat
+++ sqlserver/sqlcmd/test-sqlcmd.bat
@@ -1,1 +1,11 @@
-sqlcmd -E -S localhost -d pubs -i test.sql -o output.txt
+rem --------------------------------------------------
+rem [-E]:               Windows認証を指定
+rem [-S]:               サーバーを指定
+rem [-d pubs]:          DBを指定
+rem [-h-1]:             ヘッダを非表示
+rem [-s,]:              カンマ区切り
+rem [-W]:               余計な余白を削除
+rem [-i test.sql]:      処理するSQLファイル
+rem [-o output.txt]:    結果ファイル
+rem --------------------------------------------------
+sqlcmd -E -S localhost -d pubs -s, -W -h-1 -i test.sql -o output.txt
--- sqlserver/sqlcmd/test.sql
+++ sqlserver/sqlcmd/test.sql
@@ -1,3 +1,10 @@
-select @@servername
-select db_name()
+set nocount on -- n件処理されましたを非表示とする
+----------------------------------------
+
+-- select @@servername
+-- select db_name()
 select * from stores
+
+
+----------------------------------------
+set nocount off
