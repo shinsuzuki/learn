220949ea57fdddcba9aa67b1c1174475caca3210
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-07-31 15:25:16 +0900

    sp,func

---
+++ sqlserver/function/ufc_upper.sql
@@ -0,0 +1,15 @@
+﻿CREATE FUNCTION [dbo].[ufn_upper ]
+(
+	@TargetStr varchar(50)
+)
+RETURNS varchar(50)
+AS
+BEGIN
+  declare @resultStr varchar(50);
+
+  set @resultStr = (select upper(@TargetStr))
+
+  RETURN @resultStr;
+END
+GO
+
---
+++ sqlserver/sp/base/get_customer.sql
@@ -0,0 +1,27 @@
+﻿USE [MySample]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_get_customer]    Script Date: 2024/07/31 15:22:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_get_customer]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * from customer
+END
+GO
+
