a43c4f98a42888979d53403afe1535fc7b61cd3d
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-08-03 18:20:29 +0900

    sp add.

---
+++ sqlserver/sp/CTE（共通テーブル式）/create_sp_cte_sample.sql
@@ -0,0 +1,34 @@
+﻿-- 呼び出し
+-- EXEC sp_declare_test
+
+--------------------------------------------------------------------------------
+-- sample: CTEのサンプル
+--------------------------------------------------------------------------------
+USE [AdventureWorks2022]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- 定義済みの場合は削除
+DROP PROCEDURE IF EXISTS dbo.sp_cte_sample_test
+GO
+
+CREATE PROCEDURE sp_cte_sample_test
+AS
+BEGIN
+    -- SELECT ステートメントで取得した結果に対して名前を付けることができる機能
+    WITH tmpTable
+        as (select
+                p.BusinessEntityID as BusinessEntityID,
+                p.FirstName +' '+ p.LastName as Name,
+                e.EmailAddress
+            from Person.Person as p
+                inner join Person.EmailAddress as e on p.BusinessEntityID = e.BusinessEntityID)
+
+    select BusinessEntityID, Name, EmailAddress from tmpTable where BusinessEntityID < 100
+    RETURN
+END
+GO
---
+++ sqlserver/sp/一時テーブル/create_sp_tmp_table.sql
@@ -0,0 +1,37 @@
+﻿-- 呼び出し
+-- EXEC sp_tmp_table_test
+
+--------------------------------------------------------------------------------
+-- sample: 一時テーブル
+--------------------------------------------------------------------------------
+USE [AdventureWorks2022]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- 定義済みの場合は削除
+DROP PROCEDURE IF EXISTS dbo.sp_tmp_table_test
+GO
+
+CREATE PROCEDURE sp_tmp_table_test
+AS
+BEGIN
+    -- 一時テーブルは、次のように SELECT INTO ステートメントを利用して、SELECT ステートメントの検索結果をもとに作成することもできます。
+    select * into #tmpEmailAddress from (
+                select
+                    p.FirstName + ' ' + p.LastName as Name,
+                    e.EmailAddress
+                from
+                    Person.Person as p
+                    inner join Person.EmailAddress as e on p.BusinessEntityID = e.BusinessEntityID
+                ) as t1
+
+    select * from #tmpEmailAddress
+    drop table #tmpEmailAddress
+
+    RETURN
+END
+GO
