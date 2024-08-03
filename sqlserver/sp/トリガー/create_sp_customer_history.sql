89e2e6e8cb39d2a5bcb8865efa3f529b0823d91c
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-08-03 16:59:39 +0900

    sp add.

---
+++ sqlserver/sp/トリガー/create_sp_customer_history.sql
@@ -0,0 +1,30 @@
+﻿USE [MySample]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- 定義済みの場合は削除
+DROP TRIGGER IF EXISTS dbo.trg_customer_history_test
+GO
+
+CREATE TRIGGER trg_customer_history_test
+ON customer
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO customer_history(
+        id,
+        name,
+        created_at
+    )
+    SELECT
+        id,
+        name,
+        GETDATE()
+    FROM
+        inserted
+END
+GO
---
+++ sqlserver/sp/動的/create_sp_dynamic_sql.sql
@@ -0,0 +1,45 @@
+﻿-- 呼び出し
+-- EXEC sp_dynamic_sql_test
+
+--------------------------------------------------------------------------------
+-- sample: 動的SQL
+--------------------------------------------------------------------------------
+USE [AdventureWorks2022]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- 定義済みの場合は削除
+DROP PROCEDURE IF EXISTS dbo.sp_dynamic_sql_test
+GO
+
+CREATE PROCEDURE sp_dynamic_sql_test
+AS
+BEGIN
+    ---- 動的１（exec）
+    -- 文字として記述
+    EXEC('select top 3 * from Person.Person')
+
+    -- カラムやテーブルを変数化
+    DECLARE @tableName nvarchar(20) = 'Person.Person'
+    DECLARE @columnName nvarchar(20) = 'FirstName'
+    EXEC('select top 3 ' + @columnName + ' from ' + @tableName)
+
+    ---- 動的２（sp_executesql）
+    -- sp_executesqlはSQLの文字列連結が完了したものを設定
+    DECLARE @tableName2 nvarchar(20) = 'Person.Person'
+    DECLARE @columnName2 nvarchar(20) = 'FirstName'
+    DECLARE @sql nvarchar(100) = N'select top 3 ' + @columnName2 + ' from '  + @tableName2
+    EXEC sp_executesql @sql
+
+    -- where句にパラメータを設定
+    DECLARE @sql2 nvarchar(100) = N'select FirstName, LastName from Person.Person where FirstName like @p1 and LastName like @p2'
+    EXEC sp_executesql @sql2, N'@p1 nvarchar(50), @p2 nvarchar(50)',@p1='A%', @p2='M%'
+
+
+    RETURN
+END
+GO
