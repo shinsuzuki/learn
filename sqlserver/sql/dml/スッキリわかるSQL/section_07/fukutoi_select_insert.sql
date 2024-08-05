717d23bfb8177720e2edc4d9fb6219b93157dbac
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-08-06 03:27:32 +0900

    sql add.

--- sqlserver/sql/dml/スッキリわかるSQL/section_07/fukutoi_select_insert.sql
+++ sqlserver/sql/dml/スッキリわかるSQL/section_07/fukutoi_select_insert.sql
@@ -1,7 +1,7 @@
 ﻿use MySample
 -- 複問い合わせを利用した登録
 
---
+-- 書き方1
 declare @maxId int = (select max(id) from customer)
 
 insert into
@@ -21,3 +21,24 @@ insert into
   from
     customer
   as a
+
+
+-- 書き方2
+insert into
+  customer(
+    id,
+    name,
+    old,
+    point,
+    created_at
+  )
+  select
+    (select max(id) from customer) + ROW_NUMBER() over(order by id),  -- idを増加
+    'add_'+ a.name,
+    a.old,
+    a.point,
+    getdate()
+  from
+    customer
+  as a
+
