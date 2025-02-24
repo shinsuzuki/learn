ea448c1ef553495de6a02171745aff6eb274c201
shin.suzuki <shin.suzuki.dev@gmail.com>
2025-02-24 11:43:22 +0900

    update

--- docker/sandbox/work/docker/db/init/init-user.sql
+++ docker/sandbox/work/docker/db/init/init-user.sql
@@ -1,3 +1,9 @@
-﻿create table user (id int, name, varchar(32), email varchar(32));
+﻿DROP SCHEMA IF EXISTS sample;
+CREATE SCHEMA sample;
+USE sample;
+
+DROP TABLE IF EXISTS user;
+
+create table user (id int, name varchar(32), email varchar(32));
 insert user (id, name, email) values (1, 'John', 'john@examp.com');
 insert user (id, name, email) values (2, 'Kon', 'kon@examp.com');
