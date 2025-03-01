ba9126f4c9d5a4fbb4a67483aef959320e351e45
shin.suzuki <shin.suzuki.dev@gmail.com>
2025-03-01 23:04:58 +0900

    自己証明書作成

---
+++ tools/openssl/create.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# create dir
+echo "__create dir"
+uniqid=$(date +'%Y%m%d%H%M%S')
+mkdir "${uniqid}"
+echo "${uniqid}"
+cd "${uniqid}" || exit
+
+# create private key
+echo "__create private key"
+private_key="private_${uniqid}.key"
+openssl genrsa 2024 > "$private_key"
+
+# craete csr
+echo "__craete csr"
+server_csr="server_${uniqid}_.csr"
+openssl req -new -key "$private_key" > "$server_csr"
+
+echo "__craete crt"
+server_crt="server_${uniqid}_.crt"
+openssl x509 -req -days 398 -signkey "$private_key" < "$server_csr" > "$server_crt"
+
