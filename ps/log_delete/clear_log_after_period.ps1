07f98f24e90c16257050332f8837775ab2a36e3e
shin.suzuki <shin.suzuki.work@gmail.com>
2025-03-15 19:07:17 +0900

    経過したログを消す

---
+++ ps/log_delete/clear_log_after_period.ps1
@@ -0,0 +1,5 @@
+﻿#
+# 7日過ぎたログを消す
+#
+$DELETE_DAYS_ELAPSED = 7
+Get-ChildItem ./logs -Recurse -File | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-$DELETE_DAYS_ELAPSED) } | Remove-Item -Force
---
+++ ps/log_delete/init.ps1
@@ -0,0 +1,12 @@
+﻿# データ作成
+Remove-Item -Path logs -Recurse
+
+New-Item -Path "logs" -ItemType Directory
+New-Item ./logs/data1.log
+New-Item ./logs/data2.log
+New-Item ./logs/data3.log
+
+Set-ItemProperty ./logs/data1.log -Name CreationTime -Value (Get-Date).AddDays(-10)
+Set-ItemProperty ./logs/data1.log -Name LastWriteTime -Value (Get-Date).AddDays(-10)
+Set-ItemProperty ./logs/data2.log -Name CreationTime -Value (Get-Date).AddDays(-10)
+Set-ItemProperty ./logs/data2.log -Name LastWriteTime -Value (Get-Date).AddDays(-10)
