ba58c3b65a4908b40c618fd34d437a2faaa6cbc5
shin.suzuki <shin.suzuki.work@gmail.com>
2025-03-18 23:48:02 +0900

    aaa

---
+++ ps/_sandbox/hoge.ps1
@@ -0,0 +1,22 @@
+﻿
+# 配列(.net)
+$list = New-Object System.Collections.Generic.List[string]
+$list.Add("a")
+$list.Add("g")
+$list.Add("c")
+
+foreach ($item in $list) {
+    Write-Host $item
+}
+
+# 配列(ps)
+$array = @()
+$array += 12
+$array += 23
+Write-Host ($array -join "-")
+
+# ハッシュ
+$hash = @{}
+$hash["a"] = 1
+$hash["b"] = 2
+Write-Host $hash["b"]
