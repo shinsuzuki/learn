c093b1178813483464e1bfbabb5d9cc2dad6c2e1
shin.suzuki <shin.suzuki.work@gmail.com>
2025-03-18 23:33:59 +0900

    format

--- ps/iis/OperationIIS_appcmd_stop_AppPool.ps1
+++ ps/iis/OperationIIS_appcmd_stop_AppPool.ps1
@@ -1,7 +1,7 @@
 ﻿# 管理者権限が必要
 
 # アプリケーションプール名を設定
-$APPPOOL_NAME="DefaultAppPool"
+$APPPOOL_NAME = "DefaultAppPool"
 
 #__________ アプリケーションプール終了(appcmd)
 C:\Windows\System32\inetsrv\appcmd stop apppool /apppool.name:$APPPOOL_NAME
