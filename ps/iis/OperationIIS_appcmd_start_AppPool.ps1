713ee04a21aabdfef0a4cce7cb3aeaa8ff9aa861
shin.suzuki <shin.suzuki.work@gmail.com>
2025-03-24 21:43:55 +0900

    pwsh start-process

--- ps/iis/OperationIIS_appcmd_start_AppPool.ps1
+++ ps/iis/OperationIIS_appcmd_start_AppPool.ps1
@@ -4,4 +4,16 @@
 $APPPOOL_NAME = "DefaultAppPool"
 
 #__________ アプリケーションプール起動(appcmd)
-C:\Windows\System32\inetsrv\appcmd start apppool /apppool.name:$APPPOOL_NAME
+
+#C:\Windows\System32\inetsrv\appcmd start apppool /apppool.name:$APPPOOL_NAME
+
+#$proc = Start-Process -FilePath "$env:SYSTEMROOT\System32\inetsrv\APPCMD.exe" -ArgumentList "START APPPOOL ${APPPOOL_NAME}" -Wait -NoNewWindow -PassThru
+$proc = Start-Process -FilePath "$env:SYSTEMROOT\System32\inetsrv\APPCMD.exe" -ArgumentList "STOP APPPOOL ${APPPOOL_NAME}" -Wait -NoNewWindow -PassThru
+
+if ($proc.ExitCode -eq 0) {
+    Write-Host "success"
+} else {
+    Write-Host "error:"
+}
+
+#Write-Host "result:$result"
