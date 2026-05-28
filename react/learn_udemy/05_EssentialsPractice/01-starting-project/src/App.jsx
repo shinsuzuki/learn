c1a08cd85cb0c6493f30576e86aee2805658cb6e
shin.suzuki <shin.suzuki.work@gmail.com>
2026-05-28 16:07:42 +0900

    udemy学び

--- react/learn_udemy/05_EssentialsPractice/01-starting-project/src/App.jsx
+++ react/learn_udemy/05_EssentialsPractice/01-starting-project/src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Result from "./components/Result.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration > 0;
+
+  function handleChange(inputIdentfier, newValue) {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [inputIdentfier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Result input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
