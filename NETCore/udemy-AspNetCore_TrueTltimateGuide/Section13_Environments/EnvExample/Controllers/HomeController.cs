cf15ccb4e56ef027b8ca7853c9b178a7a057895e
shin.suzuki <shin.suzuki.work@gmail.com>
2026-07-11 18:59:08 +0900

    Udemy-NETCore_環境別の設定を学ぶ

---
+++ NETCore/udemy-AspNetCore_TrueTltimateGuide/Section13_Environments/EnvExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvExample.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            // 環境変数名を取得して表示する
+            Console.WriteLine($"Environment: {_webHostEnvironment.EnvironmentName}");
+            return Content("hello world");
+        }
+    }
+}
---
+++ NETCore/udemy-AspNetCore_TrueTltimateGuide/Section13_Environments/EnvExample/Program.cs
@@ -0,0 +1,24 @@
+namespace EnvExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                //app.UseDeveloperExceptionPage();
+                Console.WriteLine("Environment:Development");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
