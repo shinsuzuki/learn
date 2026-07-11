18a70a841af691c5339ca1eb8ccbed24b3026270
shin.suzuki <shin.suzuki.work@gmail.com>
2026-07-11 20:28:36 +0900

    コンソールアプリサンプル

---
+++ NETCore/MySample/Console/ConsoleApp/ConsoleApp1/MyService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp1
+{
+    public class MyService
+    {
+        private readonly IConfiguration _config;
+
+        public MyService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(_config["MySettings:Value"]);
+        }
+    }
+}
---
+++ NETCore/MySample/Console/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            // Configuration を作成
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // DI に登録
+            services.AddSingleton<IConfiguration>(config);
+
+            // 任意のサービスを登録
+            services.AddTransient<MyService>();
+
+            var provider = services.BuildServiceProvider();
+
+            // 実行
+            var svc = provider.GetRequiredService<MyService>();
+            svc.Run();
+        }
+
+
+    }
+}
